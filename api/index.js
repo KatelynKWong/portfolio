595d739f526e3dd281566dee8fecce884b7fa6f9
KatelynKWong <kkw004@ucsd.edu>
2025-09-12 01:08:41 -0700

    static change

--- api/index.js
+++ api/index.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const path = require("path");
-
-const app = express();
-
-// Serve static files from root public folder
-app.use(express.static(path.join(__dirname, "../public")));
-
-// SPA fallback
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-module.exports.handler = serverless(app);
+// api/index.js
+export default function handler(req, res) {
+    res.status(200).json({ message: "Hello from API!" });
+  }
+  
